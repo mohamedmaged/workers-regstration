9706950716b679b64b524bc8e19ed2a273980d02
mohamed maged <mohamedmaged35@yahoo.com>
2019-05-12 00:39:28 +0200

    fixed git ignore errors

--- public/js/app.js
+++ public/js/app.js
@@ -49514,8 +49514,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /media/mohamed/mohamedhome/anamehany/anamehani/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /media/mohamed/mohamedhome/anamehany/anamehani/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /media/mohamed/mohamedhome/anamehany/workers-regstration/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /media/mohamed/mohamedhome/anamehany/workers-regstration/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
